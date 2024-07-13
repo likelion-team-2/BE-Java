36cf76c1e5211c57fe423be083de2f75c61b8349
manhnguyen1515 <manhnguyen015.mnd@gmail.com>
2024-07-13 12:10:05 +0700

    Add translate feature

--- src/main/java/com/example/demo/service/impl/MessageServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MessageServiceImpl.java
@@ -9,13 +9,15 @@ import com.example.demo.repositories.MessageRepository;
 import com.example.demo.repositories.SessionRepository;
 import com.example.demo.repositories.UserRepository;
 import com.example.demo.service.MessageService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +30,7 @@ public class MessageServiceImpl implements MessageService {
     private final UserRepository userRepository;
     private final SessionRepository sessionRepository;
     private final RedisServiceImpl redisService;
+    private final GeminiServiceImpl geminiService;
 
     /**
      * @{inheritDoc}
@@ -39,12 +42,18 @@ public class MessageServiceImpl implements MessageService {
         if (sender.isEmpty() || session.isEmpty()) {
             return null;
         }
+
+        String apiResponse = geminiService.callApi(chatMessage.getContent());
+        JsonNode nestedNode = this.parseMessageFromJson(apiResponse);
+        String contentVi = nestedNode.path("contentVi").asText();
+        String contentKo = nestedNode.path("contentKo").asText();
+
         return messageRepository.save(Message.builder()
                 .user(sender.get())
                 .session(session.get())
                 .content(chatMessage.getContent())
-                .contentKo(chatMessage.getContent()) // TODO: Will be translated
-                .contentVi(chatMessage.getContent()) // TODO: Will be translated
+                .contentKo(contentKo)
+                .contentVi(contentVi)
                 .createdAt(LocalDateTime.now())
                 .build());
 
@@ -67,5 +76,30 @@ public class MessageServiceImpl implements MessageService {
             return null;
         }
         return lastFifteenMessages.get(lastFifteenMessages.size() - 1);
+    }
+
+    /**
+     * Parse message from response of gemini api response
+     * @param json JSON string
+     * @return parsed message
+     */
+    private JsonNode parseMessageFromJson(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            // Parse the outer JSON to get the nested JSON string
+            JsonNode rootNode = mapper.readTree(json);
+            String nestedJson = rootNode.path("candidates")
+                    .path(0)
+                    .path("content")
+                    .path("parts")
+                    .path(0)
+                    .path("text")
+                    .asText();
+
+            return mapper.readTree(nestedJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
